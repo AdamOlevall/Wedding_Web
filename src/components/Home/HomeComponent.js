324782220380539927cc402890c2d1223c6f0b0c
Adam Olevall <adamolevall@Adams-Mac.local>
2020-09-30 17:05:23 +0200

    update dates copy

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -8,7 +8,7 @@ const HomeComponent = () =>  (
   <div className="background-image">
     <div className="paper">
       <p className="header-text">Beatrice och Charles</p>
-      <p className="body-text">Välkomna till vår bröllopshelg i Vadstena 13 - 14 Augusti 2021. 
+      <p className="body-text">Välkomna till vår bröllopshelg i Vadstena 13 - 15 Augusti 2021. 
         Här nedan följer alternativa boenden som vi rekommenderar för helgen.
       </p>
       <p  className="body-text body-text-small body-text-dark">Vid frågor kontakta bruden eller brudgummen.
