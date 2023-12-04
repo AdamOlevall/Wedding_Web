d6645952bdd6906d9a0383a26060e023dd90e4d4
Adam Olevall <adamolevall@adams-mac.home>
2023-12-04 09:41:12 +0100

    test svh

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -5,7 +5,7 @@ const HomeComponent = () =>  {
   return (
     <div className="background--home">
       <div className="content--home">
-              <p className="header-text--home">Adam & Eva</p>
+              <p className="header-text--home">Adam & Matilda</p>
               <p className="header-text-small--home">2024.08.24</p>
       </div>
     </div>
