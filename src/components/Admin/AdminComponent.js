77194cc7a111d83004418719fa9c45bd4a256ec3
Adam Olevall <adamolevall@adams-mac.home>
2024-02-04 10:42:47 +0100

    fix scroll for admin page

--- src/components/Admin/AdminComponent.js
+++ src/components/Admin/AdminComponent.js
@@ -15,36 +15,40 @@ const AdminComponent = () => {
 
     return (
         <div className="background--admin">
-            <TextField
-                            id="firstName"
-                            label="Sök person"
-                           value={searchValue}
-                           onChange={e => setSearchValue(e.target.value)}
-                        />
-                    <div className="attenders">
-                        <p className="attender-heading">{attenders.length} svar</p>
-                        <p className="attender-heading">{attenders.filter(attender => attender.weddingDay === 'ja').length} Ja</p>
-                        <p className="attender-heading">{attenders.filter(attender => attender.transport === 'ja').length} Ja</p>
-                        <p className="attender-heading"></p>
-                            <p className="attender-heading">Namn</p>
-                            <p className="attender-heading">Bröllopsdagen</p>
-                            <p className="attender-heading">Transport</p>
-                            <p className="attender-heading">Allergi och matpreferenser</p>
-                           
+             <div className="scroll-area--admin">
+                <div className="content--admin">
+                    <TextField
+                        id="firstName"
+                        label="Sök person"
+                        value={searchValue}
+                        onChange={e => setSearchValue(e.target.value)}
+                    />
+                        <div className="attenders">
+                            <p className="attender-heading">{attenders.length} svar</p>
+                            <p className="attender-heading">{attenders.filter(attender => attender.weddingDay === 'ja').length} Ja</p>
+                            <p className="attender-heading">{attenders.filter(attender => attender.transport === 'ja').length} Ja</p>
+                            <p className="attender-heading"></p>
+                                <p className="attender-heading">Namn</p>
+                                <p className="attender-heading">Bröllopsdagen</p>
+                                <p className="attender-heading">Transport</p>
+                                <p className="attender-heading">Allergi och matpreferenser</p>
+                            
 
-                        {
-                            attenders
-                            .filter(val => `${val.firstName.toLowerCase()}  ${val.lastName.toLowerCase()}`.includes(searchValue.toLowerCase()))
-                            .map(attender => (
-                                <React.Fragment>
-                                    <p>{attender.firstName} {attender.lastName}</p>
-                                    <p>{attender.weddingDay}</p>
-                                    <p>{attender.transport}</p>
-                                    <p>{attender.allergy}</p>
-                                </React.Fragment>
+                            {
+                                attenders
+                                .filter(val => `${val.firstName.toLowerCase()}  ${val.lastName.toLowerCase()}`.includes(searchValue.toLowerCase()))
+                                .map(attender => (
+                                    <React.Fragment>
+                                        <p>{attender.firstName} {attender.lastName}</p>
+                                        <p>{attender.weddingDay}</p>
+                                        <p>{attender.transport}</p>
+                                        <p>{attender.allergy}</p>
+                                    </React.Fragment>
 
-                            ))
-                        }
+                                ))
+                            }
+                        </div>
+                </div>         
              </div>
         </div>
     );
