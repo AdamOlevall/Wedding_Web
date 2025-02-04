d1f1010d167f54c2f2b926d53a355cf0d6693d89
Adam Olevall <adamolevall@Adams-Mac.local>
2025-02-04 08:31:30 +0100

    added sorting to adming

--- src/components/Admin/AdminComponent.js
+++ src/components/Admin/AdminComponent.js
@@ -6,6 +6,7 @@ import './style.css';
 const AdminComponent = () => {
    const [attenders, setAttenders] = useState([]);
    const[searchValue, setSearchValue] = useState("");
+   const [sortOnName, setSortOnName] = useState(false);
     useEffect(() => {
         getAttenders()
         .then(data => {
@@ -13,18 +14,21 @@ const AdminComponent = () => {
         })
     }, []);
 
-    const sortedAttendersOnName = attenders.sort((a, b) => a.firstName.localeCompare(b.firstName))
-
+    const sortedAttendersOnName = [...attenders].sort((a, b) => a.firstName.localeCompare(b.firstName))
+    
     return (
         <div className="background--admin">
              <div className="scroll-area--admin">
                 <div className="content--admin">
+                    <div style={{padding: "0 8px", width: "100%", display:"flex", justifyContent: "space-between", alignItems:"center"}}>
                     <TextField
                         id="firstName"
                         label="Sök person"
                         value={searchValue}
                         onChange={e => setSearchValue(e.target.value)}
                     />
+                    <button style={{padding: "4px", background: "#616bdd", borderRadius: "4px", cursor: "pointer"}} onClick={() => sortOnName ? setSortOnName(false) : setSortOnName(true)}>Sortera på {sortOnName ? "nyast" : "förnamn"}</button>
+                    </div>
                         <div className="attenders">
                             <p className="attender-heading">{attenders.length} svar</p>
                             <p className="attender-heading">{attenders.filter(attender => attender.mingle === 'ja').length} Ja</p>
@@ -37,7 +41,7 @@ const AdminComponent = () => {
                                 <p className="attender-heading">Barn Mingel</p>
                                 <p className="attender-heading">Allergi och matpreferenser</p>
                             {
-                                sortedAttendersOnName
+                               (sortOnName ? sortedAttendersOnName : attenders)
                                 .filter(val => `${val.firstName.toLowerCase()}  ${val.lastName.toLowerCase()}`.includes(searchValue.toLowerCase()))
                                 .map(attender => (
                                     <React.Fragment key={`${attender.firstName} ${attender.lastName}`}>
