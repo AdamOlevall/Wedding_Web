4e29e6f7008eee0711336fc3cd335233e3c4d332
Adam Olevall <adamolevall@Adams-Mac.lan>
2025-07-07 21:06:03 +0200

    edit typo

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -6,10 +6,10 @@ const HomeComponent = () =>  {
     <div className="background--home">
       <div className="scroll-area--home">
         <div className="content--home">
-                <p className="new-info-header--home">Nya viktig info</p>
+                <p className="new-info-header--home">Ny viktig info</p>
                 <p className="new-info-text--home">
 Det kommer finnas minibussar som går hem till Västerås under natten. Bussarna börjar rulla ca 01. Exakta tider bestäms på plats för att fylla bussarna på bästa sätt. Sista bussen går ca 04. 
-Pris: 100 kr/person. För att boka plats på en buss: Smsa Maja på 076-2557990 senast 19/7. Bussarna kör in till centrala Västerås (ej hem till dörren).</p>
+Pris: 100 kr/person. För att boka plats på en buss: swisha Maja på 076-2557990 senast 19/7. Bussarna kör in till centrala Västerås (ej hem till dörren).</p>
                 <p className="new-info-text--home">
                 Om vädret är katastrof under fredagen så kommer minglet att ställas in. Vi håller tummarna för att vädergudarna är med oss, men om så inte är fallet kommer vi kontakta er.
                 </p>
