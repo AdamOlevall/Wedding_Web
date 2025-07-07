ba34a22c68d235da5640d8512962a9f8cd98e367
Adam Olevall <adamolevall@Adams-Mac.lan>
2025-07-07 20:58:50 +0200

    new info

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -4,9 +4,16 @@ import './style.css';
 const HomeComponent = () =>  {
   return (
     <div className="background--home">
-      <div className="content--home">
-              <p className="header-text--home">Maja och Robin</p>
-              <p className="sub-header-text--home">25:e - 26:e Juli 2025</p>
+      <div className="scroll-area--home">
+        <div className="content--home">
+                <p className="new-info-header--home">Nya viktig info</p>
+                <p className="new-info-text--home">
+Det kommer finnas minibussar som går hem till Västerås under natten. Bussarna börjar rulla ca 01. Exakta tider bestäms på plats för att fylla bussarna på bästa sätt. Sista bussen går ca 04. 
+Pris: 100 kr/person. För att boka plats på en buss: Smsa Maja på 076-2557990 senast 19/7. Bussarna kör in till centrala Västerås (ej hem till dörren).</p>
+                <p className="new-info-text--home">
+                Om vädret är katastrof under fredagen så kommer minglet att ställas in. Vi håller tummarna för att vädergudarna är med oss, men om så inte är fallet kommer vi kontakta er.
+                </p>
+        </div>
       </div>
     </div>
   );
